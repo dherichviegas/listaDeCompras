8f902a625241dc28eb156b95ac92e5df82c75abf
Dherich Viegas <dherichviegas@gmail.com>
2021-09-27 21:26:16 -0300

    adicionar index script e style

---
+++ script.js
@@ -0,0 +1,43 @@
+let input  = document.getElementById('item-input');
+let button = document.getElementById('item-button');
+let ul = document.getElementById('items');
+let items = document.getElementsByTagName('li');
+let itemButtons = document.getElementsByClassName('remove');
+
+function criarElemento(){
+
+let li = document.createElement('li');
+let mensagem = "";
+let xButton = document.createElement('button');   
+    xButton.innerHTML = 'X';
+    xButton.className = 'remove';
+   
+    li.appendChild(xButton);
+let text = document.createTextNode(input.value);
+    
+    li.appendChild(text);
+//li.innerHTML = input.value;
+     
+    ul.appendChild(li);  
+    input.value = "";
+ 
+buttonEvents();
+
+}
+
+function deleteItem(){
+this.parentElement.remove();
+}
+
+
+function buttonEvents(){
+    
+  
+    for (let i = 0; i < itemButtons.length; i++)
+     itemButtons[i].addEventListener('click', deleteItem)
+ 
+   
+}
+
+button.addEventListener('click', criarElemento);
+buttonEvents();
